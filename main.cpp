d649e4ff4f914b17c1b17d56f4e6baabb893e000
atrest890 <swarovski991@gmail.com>
2019-10-17 11:58:15 +0700

    Rewrite output (Build -> build)

--- main.cpp
+++ main.cpp
@@ -2,7 +2,7 @@
 #include "version.h"
 
 int main() {
-    std::cout << "Build " << PROJECT_VERSION_PATCH << std::endl;
+    std::cout << "build " << PROJECT_VERSION_PATCH << std::endl;
     std::cout << "Hello, World!" << std::endl;
 
     return 0;
